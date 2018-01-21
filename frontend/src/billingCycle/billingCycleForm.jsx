78173198cf620ecbe37641b6ce6de47817cf7444
Tiago Nogueira <tiagonogueira@hi-interactive.pt>
2018-01-21 22:37:14 +0000

    aula 149

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
@@ -9,14 +9,14 @@ import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
   render() {
-    const { handleSubmit, readOnly } = this.props
+    const { handleSubmit, readOnly, credits } = this.props
     return (
       <form role='form' onSubmit={ handleSubmit }>
         <div className='box-footer'>
           <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Digite o Nome' />
           <Field name='month' component={LabelAndInput} readOnly={readOnly} label='Mês' cols='12 4' placeholder='Digite o Mês' />
           <Field name='year' component={LabelAndInput} readOnly={readOnly} label='Ano' cols='12 4' placeholder='Digite o Ano' />
-          <CreditList cols='12 6' readOnly={readOnly} />
+          <CreditList cols='12 6' list={credits} readOnly={readOnly} />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -28,5 +28,8 @@ class BillingCycleForm extends Component {
 }
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
