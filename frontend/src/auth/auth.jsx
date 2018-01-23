1930a2dc670dbf690ab5fc8c7604adb560541f80
Tiago Nogueira <tiagonogueira@hi-interactive.pt>
2018-01-23 23:29:57 +0000

    aula 172

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -29,7 +29,29 @@ class Auth extends Component {
     const { loginMode } = this.state
     const { handleSubmit } = this.props
     return (
-  
+      <div className="login-box">
+        <div className="login-logo">
+          <b> My</b> Money</div>
+        <div className="login-box-body">
+          <p className="login-box-msg">Bem vindo!</p>
+          <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
+            <Field component={Input} type="input" name="name" placeholder="Nome" icon='user' hide={loginMode} />
+            <Field component={Input} type="email" name="email" placeholder="E-mail" icon='envelope' />
+            <Field component={Input} type="password" name="password" placeholder="Senha" icon='lock' />
+            <Field component={Input} type="password" name="confirm_password" placeholder="Confirmar Senha" icon='lock' hide={loginMode} />
+            <Row>
+              <Grid cols="4">
+                <button type="submit" className="btn btn-primary btn-block btn-flat">
+                  {loginMode ? 'Entrar' : 'Registrar'}   
+                </button>
+              </Grid>
+            </Row>
+          </form>
+          <br />
+          <a onClick={()=> this.changeMode()}>{loginMode ? 'Novo usuário? Registrar aqui!' : 'Já é cadastrado? Entrar aqui!'}</a>
+        </div>
+        <Messages />
+      </div>
     )
   }
 }
