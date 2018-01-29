84da009503818777a83cf39c9618db1779741cb3
Tiago Nogueira <tiagonogueira@hi-interactive.pt>
2018-01-29 18:58:00 +0000

    fix problems with Routs

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import AuthOrApp from './main/authOrApp'
+import Routes from './main/routes'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
@@ -16,6 +16,6 @@ const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devT
 
 ReactDOM.render(
   <Provider store={store}>
-    <AuthOrApp />
+    <Routes />
   </Provider>
 , document.getElementById('app'))
